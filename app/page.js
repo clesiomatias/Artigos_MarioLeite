84f9d4d826c5d466d61d911f5a16821043932ce3
clesiomatias <clesiofmatias@gmail.com>
2024-06-20 17:07:35 -0300

    correções do posicionamento das seções

--- app/page.js
+++ app/page.js
@@ -8,13 +8,13 @@ export default function Home() {
     <div>
       <NavBar />
       <section
-        id="about"
+        id="codes"
         className="text-white h-screen bg-cyan-600  text-center flex"
       >
-        <h1 className="text-3xl md:text-8xl m-auto font-waterfall text-7xl text-white font-bold  flex flex-col items-center space-y-2.5">
-          <Link href={"/about"}>Sobre</Link>
+        <h1 className="text-3xl md:text-8xl m-auto font-waterfall text-7xl text-white font-bold flex flex-col items-center space-y-2.5">
+          <Link href={"/codes"}>Textos e Códigos</Link>
           <p className="text-center font-semibold text-lg md:textbase sm:text-sm">
-            Conheça um pouco da história do Prof. Mário Leite.
+            Aqui você pode acompanhar os textos e códigos lançados semanalmente!
           </p>
         </h1>
       </section>
@@ -29,19 +29,10 @@ export default function Home() {
       </section>
 
       <section
-        id="codes"
-        className="text-white h-screen bg-cyan-600  text-center flex"
+        id="courses"
+        className=" text-white h-screen bg-cyan-600  text-center flex"
       >
-        <h1 className="text-3xl md:text-8xl m-auto font-waterfall text-7xl text-white font-bold flex flex-col items-center space-y-2.5">
-          <Link href={"/codes"}>Textos e Códigos</Link>
-          <p className="text-center font-semibold text-lg md:textbase sm:text-sm">
-            Aqui você pode acompanhar os textos e códigos lançados semanalmente!
-          </p>
-        </h1>
-      </section>
-
-      <section id="courses" className=" text-cyan-900 h-screen bg-sky-300 flex">
-        <h1 className="text-3xl md:text-8xl m-auto font-waterfall text-7xl text-cyan-900 font-bold flex flex-col items-center space-y-2.5">
+        <h1 className="text-3xl md:text-8xl m-auto font-waterfall text-7xl text-cwhite font-bold flex flex-col items-center space-y-2.5">
           <Link href={"/courses"}>Minicursos</Link>
           <p className="text-center font-semibold text-lg md:textbase sm:text-sm">
             Tenha acesso a minicursos e aulas, aumentando seu conhecimento!
@@ -58,6 +49,14 @@ export default function Home() {
           <p className="text-center font-semibold text-lg md:textbase sm:text-sm">
             O Prof. Mário Leite disponibiliza para baixar diversos arquivos como
             material de estudo e complemento dos cursos e outros!
+          </p>
+        </h1>
+      </section>
+      <section id="about" className="text-cyan-900 h-screen bg-sky-300 flex">
+        <h1 className="text-3xl md:text-8xl m-auto font-waterfall text-7xl  text-cyan-900  font-bold  flex flex-col items-center space-y-2.5">
+          <Link href={"/about"}>Sobre</Link>
+          <p className="text-center font-semibold text-lg md:textbase sm:text-sm">
+            Conheça um pouco da história do Prof. Mário Leite.
           </p>
         </h1>
       </section>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,22 +25,16 @@ function NavBar() {
             <div className="hidden md:block">
               <div className="ml-4 flex items-center space-x-4">
                 <a
-                  href="/about"
+                  href="/codes"
                   className="text-white hover:bg-white hover:text-black px-3 py-2 rounded-lg"
                 >
-                  Sobre
+                  Códigos e Textos
                 </a>
                 <a
                   href="/books"
                   className="text-white hover:bg-white hover:text-black px-3 py-2 rounded-lg"
                 >
                   Livros
-                </a>
-                <a
-                  href="/codes"
-                  className="text-white hover:bg-white hover:text-black px-3 py-2 rounded-lg"
-                >
-                  Códigos e Textos
                 </a>
                 <a
                   href="/courses"
@@ -53,6 +47,12 @@ function NavBar() {
                   className="text-white hover:bg-white hover:text-black px-3 py-2 rounded-lg"
                 >
                   Arquivos para baixar
+                </a>
+                <a
+                  href="/about"
+                  className="text-white hover:bg-white hover:text-black px-3 py-2 rounded-lg"
+                >
+                  Sobre
                 </a>
               </div>
             </div>
@@ -101,22 +101,16 @@ function NavBar() {
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               
                 <a
-                  href="/about"
+                  href="/codes"
                   className="text-white block hover:bg-white hover:text-black px-3 py-2 rounded-lg"
                 >
-                  Sobre
+                  Códigos e Textos
                 </a>
                 <a
                   href="/books"
                   className="text-white block hover:bg-white hover:text-black px-3 py-2 rounded-lg"
                 >
                   Livros
-                </a>
-                <a
-                  href="/codes"
-                  className="text-white block hover:bg-white hover:text-black px-3 py-2 rounded-lg"
-                >
-                  Códigos e Textos
                 </a>
                 <a
                   href="/courses"
@@ -131,6 +125,12 @@ function NavBar() {
                   Arquivos para baixar
                 </a>
               
+                <a
+                  href="/about"
+                  className="text-white block hover:bg-white hover:text-black px-3 py-2 rounded-lg"
+                >
+                  Sobre
+                </a>
             </div>
           </div>
         )}
