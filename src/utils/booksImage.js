57e4d8c87c41fb8b6953fe91f831834b52623e5a
clesiomatias <clesiofmatias@gmail.com>
2024-06-20 17:13:40 -0300

    correções do posicionamento das seções

--- src/utils/booksImage.js
+++ src/utils/booksImage.js
@@ -1,10 +1,10 @@
 export const ImageBooks = {
   1: {
-    url: "/imgLivros/1.PNG",
-    title: "1001 Programas Prontos",
+    url: "/imgLivros/10.png",
+    title: "Linguagem C#",
     description:
-      "Este livro tem como objetivo auxiliar todos os programadores, de todas  as linguagens, na criação de soluções imediatas e práticas para vários  tipos de problemas relacionados à codificação de programas de  computador. Diferente de todas as obras sobre o ensino de programação,  este livro apresenta 1001 (mil e um) exemplos práticos de programas, em  pseudocódigos, que podem ser convertidos na linguagem que o programador domine, sem muito esforço, já que a obra é totalmente didática e direcionada, justamente, para evitar complexidades desnecessárias na solução dos problemas. Assim, rompe com o paradigma tradicional do ensino de programação, mostrando um método mais prático e mais eficiente, uma vez que não se prende à uma linguagem especifica, deixando o programador basear-se na solução apresentada em pseudocódigo para criar seus próprios programas na sua linguagem preferida. É uma obra inédita no meio editorial de livros sobre programação, pois é algo novo no sentido de ensinar a programar “programando” e não “codificando”, enfatizando a importância da Lógica de Programação, em vez de se concentrar em uma linguagem específica. Isto faz com que sejam abordados todos os tipos de problemas comerciais, científicos e acadêmicos, que podem ser resolvidos através de programas de computador. Os assuntos abordados vão desde uma simples média até otimização de processos, passando por cálculos matriciais, cálculos financeiros, maximização, estatística, controle escolar, álgebra linear, cálculos geométricos, teoria dos números, randomização, criptografia, séries, integrais, derivadas, limites, transformações de bases, funções polinomiais, regressões, tratamento de strings, recursividade, modularização de programas, e muitos outros. Todos estes assuntos são bem colocados nos programas, com enfoque didático e comentados na medida certa, e muito fáceis de entender e aplicar.",
-    buy: "https://www.amazon.com.br/Programas-Prontos-Codificar-Linguagem-Preferida-ebook/dp/B088PYZLQK",
+      "Este livro é direcionado a todos os programadores: iniciantes,  intermediários e experientes que desejam migrar para o C# ou apenas  estudar e conhecer mais uma linguagem de programação profissional. Além  de apresentar os recursos básicos da linguagem nos dez capítulos  iniciais com figuras, esquemas, exemplos e exercícios aborda um assunto  fundamental para qualquer profissional de programação nos cinco  capítulos finais: acesso a cinco diferentes tipos de bancos de dados.  Este é um diferencial muito importante quando se trata de mostrar, de  maneira prática e objetiva, o assunto mais importante no desenvolvimento  de sistemas: a extração de informações armazenadas em bases de dados. E  sendo uma linguagem orientada a objetos e a eventos, a obra explora  muito bem estas tecnologias nas aplicações de maneira didática e sem  precisar introduzir complexidades desnecessárias.",
+    buy: "https://clubedeautores.com.br/livro/linguagem-c-2",
   },
   2: {
     url: "/imgLivros/2.PNG",
@@ -14,59 +14,60 @@ export const ImageBooks = {
     buy: "https://www.amazon.com.br/Programa%C3%A7%C3%A3o-B%C3%A1sica-Visualg-Antonio-Nicolodi/dp/6587030017",
   },
   3: {
+    url: "/imgLivros/1.PNG",
+    title: "1001 Programas Prontos",
+    description:
+      "Este livro tem como objetivo auxiliar todos os programadores, de todas  as linguagens, na criação de soluções imediatas e práticas para vários  tipos de problemas relacionados à codificação de programas de  computador. Diferente de todas as obras sobre o ensino de programação,  este livro apresenta 1001 (mil e um) exemplos práticos de programas, em  pseudocódigos, que podem ser convertidos na linguagem que o programador domine, sem muito esforço, já que a obra é totalmente didática e direcionada, justamente, para evitar complexidades desnecessárias na solução dos problemas. Assim, rompe com o paradigma tradicional do ensino de programação, mostrando um método mais prático e mais eficiente, uma vez que não se prende à uma linguagem especifica, deixando o programador basear-se na solução apresentada em pseudocódigo para criar seus próprios programas na sua linguagem preferida. É uma obra inédita no meio editorial de livros sobre programação, pois é algo novo no sentido de ensinar a programar “programando” e não “codificando”, enfatizando a importância da Lógica de Programação, em vez de se concentrar em uma linguagem específica. Isto faz com que sejam abordados todos os tipos de problemas comerciais, científicos e acadêmicos, que podem ser resolvidos através de programas de computador. Os assuntos abordados vão desde uma simples média até otimização de processos, passando por cálculos matriciais, cálculos financeiros, maximização, estatística, controle escolar, álgebra linear, cálculos geométricos, teoria dos números, randomização, criptografia, séries, integrais, derivadas, limites, transformações de bases, funções polinomiais, regressões, tratamento de strings, recursividade, modularização de programas, e muitos outros. Todos estes assuntos são bem colocados nos programas, com enfoque didático e comentados na medida certa, e muito fáceis de entender e aplicar.",
+    buy: "https://www.amazon.com.br/Programas-Prontos-Codificar-Linguagem-Preferida-ebook/dp/B088PYZLQK",
+  },
+  4:{
     url: "/imgLivros/3.PNG",
     title: "Curso Básico de Programação",
     description:
       "Este livro aborda a programação para computadores digitais de uma  maneira moderna, prática e didática, com o objetivo de introduzir o  leitor ao fantástico mundo da computação eletrônica. A obra foi escrita  para proporcionar ao leitor uma visão ampla e objetiva do assunto,  utilizando um método de fácil assimilação e aplicação, com uma abordagem  simples e abstraindo das complexidades muito comuns em obras  especializadas nesta área. Embora já existam várias publicações sobre  este assunto, o livro tem diferenciais que ajudam a programar de uma  maneira geral, sem se preocupar com uma linguagem em particular: apenas  baseando no raciocínio lógico traduzido em pseudocódigo. Um desses  diferenciais está no fato de trabalhar com figuras, esquemas, exemplos  comentados e exercícios propostos ao final de cada um dos seus nove  capítulos. Os tópicos abordam todos os assuntos importantes para um  perfeito aprendizado, incluindo: tipos de dados, operadores, variáveis,  constantes, arrays, registros, estruturas de controle, rotinas  (procedimentos e funções), passagens de parâmetros, modularização, etc.  Todos esses assuntos são tratados de um modo bem didático com bastante  simplicidade e objetividade, mas sem introduzir sofisticações  desnecessárias. E para reforçar o aprendizado é apresentado um capítulo  inteiramente dedicado a uma ferramenta de implementação de algoritmo (o  VisuAlg) para que o leitor possa 'rodar' seus algoritmos de maneira  prática, rápida e fácil.",
     buy: "https://www.amazon.com.br/Curso-B%C3%A1sico-Programa%C3%A7%C3%A3o-Teoria-Pr%C3%A1tica/dp/8539908700",
-  },
-  4: {
+  }, 
+  5:{
     url: "/imgLivros/4.PNG",
     title: "Curso Básico de C",
     description:
       "Este livro aborda a linguagem C de uma maneira moderna e sem  sofisticações com o objetivo de introduzir o leitor nessa maravilhosa  ferramenta de programação. Foi escrito para proporcionar uma visão ampla  da linguagem e ao mesmo tempo situá-la num contexto didático com uma  abordagem simples abstraindo das complexidades muito comuns em obras  especializadas nesse assunto. E embora existam vários livros e  publicações sobre esta linguagem este livro tem diferenciais que ajudam  o candidato a programador a tomar a decisão de abraçar o C como sua  linguagem base. O principal diferencial está no fato de trabalhar com  figuras esquemas exemplos comentados e exercícios propostos ao longo dos  seus onze capítulos e para isto a obra foi baseada em aulas ministradas  em cursos técnicos de programação e de graduação em Ciência da  Computação e engenharias. Este livro foi escrito para ajudar o  programador a considerar o C como uma linguagem que pode ser dominada  sem recorrer a sofisticações desnecessárias.",
     buy: "https://www.amazon.com.br/Curso-Basico-Pratico-F%C3%A1cil-Leite/dp/8539903377",
   },
-  5: {
+  6: {
     url: "/imgLivros/5.PNG",
     title: "SciLab, Uma abordagem Prática e Didática",
     description:
       "Esta segunda edição sobre SciLab é uma revisão da edição anterior,  ampliada e atualizada com um enfoque mais abrangente. A obra trata de  diversos assuntos da ferramenta de maneira fácil de entender, podendo  ser consultada por todos que necessitam de uma ferramenta que responda  às questões numéricas de forma clara e objetiva, permitindo tomadas de  decisão em cima de resultados numéricos. Nesta edição estão incluídos  novos assuntos bem explicados, através de exemplos simples, figuras e  esquemas. Entre esses novos assuntos podem ser destacados: resolução  numérica de equações diferenciais, cálculos de séries, otimização pelo  método Simplex e novos scripts de programas, além de um capítulo  especial sobre o toolbox Xcos em nível introdutório. Criação de  polinômios, determinação de zeros de funções polinomiais, valor da  função num dado ponto e operações gerais também estão presentes. As  matrizes e vetores são abordados de maneira fácil e inteligível, com  assuntos relacionados à Álgebra Linear e suas implicações teóricas e  práticas, tais como: regressão linear e polinomial, resolução de  sistemas de equações lineares e não lineares, triangulação, operações  matriciais diversas, produto escalar, produto vetorial, produto misto,  leitura de matriz em arquivo, listas, etc. Todos estes assuntos são  mostrados de maneira fácil, objetiva e didática.",
     buy: "https://www.amazon.com.br/SciLab-Uma-Abordagem-Pr%C3%A1tica-Did%C3%A1tica/dp/8539906570",
   },
-  6: {
+  7: {
     url: "/imgLivros/6.PNG",
     title: "Acessando Banco de Dados - Delphi",
     description:
       "Este livro ensina como acessar as principais bases de dados utilizadas  nos sistemas de informação de maneira prática e objetiva, através de  exemplos em Delphi 7 e Delphi 2007. São acessadas, pela ordem: dBase,  Paradox, Access, Interbase, Firebird, MySQL, Oracle, PostgreSQL e SQL  Server, nas suas versões mais atualizadas e através de diferentes  provedores. São apresentadas técnicas de conexão e acessos às bases de  dados através de aplicações práticas e bem didáticas, abrangendo as  tecnologias ADO, ODBC e dbExpress. Também são empregados componentes e  provedores externos, tais como Zeos e MyDac e outros, no sentido de  ampliar ainda mais as possibilidades de aprendizado. É um livro de fácil  assimilação, que apresenta diversas ferramentas de gerenciamento das  bases de dados: Database Desktop, Access, IBConsole, IBExpert,  MySQL-Front, pgAdmin, SQL*Plus, iSQL*Plus e SQL Server Manager. Quando  há necessidade, a ferramenta é trabalhada desde o download até seu uso  interativo, passando pelas etapas mais importantes da instalação. E para  cada uma delas o livro mostra como criar bancos, tabelas, índices,  restrições e outros elementos e recursos, com base em exemplos do Modelo  Relacional. Os exemplos de aplicações de acesso às bases de dados  utilizam as versões mais atuais do Delphi com exemplos bem objetivos e  extremamente didáticos, e amplamente comentados nas codificações,  realçando os pontos mais importantes de conexão/acesso às tabelas. O  resultado final é uma obra de fácil entendimento, sem sofisticações  desnecessárias, direcionada aos programadores iniciantes, médios e até  profissionais que buscam diversificar os tipos de conexões empregadas  nos sistemas, através de aplicações desenvolvidas com ferramentas RAD.",
     buy: "",
   },
-  7: {
+  8: {
     url: "/imgLivros/7.PNG",
     title: "Acessando Banco de Dados - Visual Basic",
     description:
       "Este livro mostra como acessar as bases de dados mais utilizadas nos  sistemas de informação de modo prático e objetivo. As bases de dados  estudadas e acessadas são, pela ordem: dBase, Paradox, Access,  Interbase, Firebird, MySQL Oracle, PostgreSQL e SQL Server através de  diferentes provedores. São apresentadas técnicas de conexão às diversas  bases de dados através de aplicações em Visual Basic (VB6 e VB.NET) de  maneira simples e prática, empregando as tecnologias Jet, DAO, ODBC, ADO  e ADO.NET envolvendo componentes predefinidos e instanciados através de  código puro além de acessos diretos via instruções SQL.",
     buy: "",
   },
-  8: {
+  9: {
     url: "/imgLivros/8.PNG",
     title: "Técnicas de Programação",
     description:
       "Este livro mostra como elaborar programas bem estruturados e modulares,  de modo que qualquer correção seja fácil e sem complicações. São  apresentadas as técnicas de programação estruturada, modular, orientada  a eventos e orientada a objetos, focalizadas de maneira bem didática e  objetiva, sem introduzir complexidades desnecessárias. O objetivo do  autor é apresentar um trabalho no qual estudantes dos cursos de  Processamento de Dados, Ciência da Computação, Sistemas de Informação e  Administração de Sistemas possam ter uma referência sobre técnicas de  programação, dentro de uma visão moderna e atualizada, baseada em  exemplos práticos e objetivos, incluindo a técnica de Programação  Orientada a Objetos, com um exemplo bem explicado e objetivo.",
     buy: "",
   },
-  9: {
+  10: {
     url: "/imgLivros/9.PNG",
     title: "Programação Básica - Delphi",
     description:
       "A obra é, antes de tudo, um trabalho objetivo que procura unir a  didática com a prática necessária ao desenvolvimento de aplicações,  construído em sala de aula com exemplos do dia-a-dia, pesquisas,  esclarecimentos de dúvidas e sugestões de alunos e professores. Com  relação à versão do Delphi, o autor esclarece que, desde a primeira  edição dos textos utilizados em aula, o objetivo não foi o de trabalhar  com uma nova versão em particular e sim, manter os assuntos e recursos  básicos da ferramenta, aliando teoria e prática. Mesmo porque o  núcleo-base da ferramenta não mudou essencialmente com o tempo; contudo,  muitas das novidades surgidas não ficaram de fora desta edição, só pra  citar algumas: arrays dinâmicos, lista de tarefas To-Do List, constantes  tipificadas e geração de relatórios. Também é apresentado um capítulo  inteiro sobre os fundamentos de Análise de Sistemas, com um exemplo  completo de Modelagem de Dados baseado na Análise Essencial (com  Diagrama de Contexto, DFDs, Lista de Tarefas, Requisitos de Sistema,  etc). Também são apresentas as soluções dos exercícios propostos ao  longo do livro, criação de DLL ́s, e muito mais. Portanto, trata-se de um  livro de interesse e utilidade aos programadores, abrangendo os níveis  básico, intermediário e avançado.",
     buy: "",
   },
-  10: {
-    url: "/imgLivros/10.png",
-    title: "Linguagem C#",
-    description:
-      "Este livro é direcionado a todos os programadores: iniciantes,  intermediários e experientes que desejam migrar para o C# ou apenas  estudar e conhecer mais uma linguagem de programação profissional. Além  de apresentar os recursos básicos da linguagem nos dez capítulos  iniciais com figuras, esquemas, exemplos e exercícios aborda um assunto  fundamental para qualquer profissional de programação nos cinco  capítulos finais: acesso a cinco diferentes tipos de bancos de dados.  Este é um diferencial muito importante quando se trata de mostrar, de  maneira prática e objetiva, o assunto mais importante no desenvolvimento  de sistemas: a extração de informações armazenadas em bases de dados. E  sendo uma linguagem orientada a objetos e a eventos, a obra explora  muito bem estas tecnologias nas aplicações de maneira didática e sem  precisar introduzir complexidades desnecessárias.",
-    buy: "https://clubedeautores.com.br/livro/linguagem-c-2",
-  },
+  
 };
