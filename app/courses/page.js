192a416033156da0ef6da8eec8a41ef253b11bd9
clesiomatias <clesiofmatias@gmail.com>
2024-07-21 17:32:15 -0300

    Inserindo pagina de cursos

--- app/courses/page.js
+++ app/courses/page.js
@@ -1,17 +1,71 @@
 "use client";
 import NavBar from "@/src/components/NavBar";
-import Image from "next/image";
-import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+
+import { Card, CardHeader, CardFooter } from "@nextui-org/card";
 import "../globals.css";
 import { useState } from "react";
+import Image from "next/image";
 const Courses = () => {
   return (
     <>
       <NavBar />
       <div className="w-full h-full min-h-screen flex flex-col p-10 items-center justify-center bg-cyan-400 mt-1">
-        <h1 className="animate-bounce text-2xl font-bold text-cyan-900">
-          Em Breve!!!
-        </h1>
+        <h1 className="text-2xl font-bold text-cyan-900">Minicursos</h1>
+
+        <div className="flex p-10 items-center justify-center gap-10">
+          <a href="https://github.com/ProfMLE/Rep02" target="_blank">
+            <Card className="flex items-center">
+              <CardHeader>
+                <Image
+                  alt="Imagem ícone do curso de Lógica de Programação"
+                  className="object-cover rounded-xl"
+                  src="/imgs/logica.png"
+                  width={270}
+                  height={270}
+                />
+              </CardHeader>
+              <CardFooter className="flex items-center justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 bg-white ">
+                <h1 className="text-cyan-900 text-bold">
+                  {" "}
+                  Lógica de Programação
+                </h1>
+              </CardFooter>
+            </Card>
+          </a>
+          <a href="https://github.com/ProfMLE/Rep004" target="_blank">
+          <Card>
+            <CardHeader>
+              <Image
+                alt="Imagem ícone do curso de Lógica de Programação"
+                className="object-cover rounded-xl"
+                src="/imgs/banco de dados.png"
+                width={270}
+                height={270}
+                />
+            </CardHeader>
+            <CardFooter className="flex items-center justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 bg-white ">
+              <h1 className="text-cyan-900 text-bold"> Banco de Dados</h1>
+            </CardFooter>
+          </Card>
+                </a>
+          <a href="https://github.com/ProfMLE/Rep004" target="_blank">
+          
+          <Card>
+            <CardHeader>
+              <Image
+                alt="Imagem ícone do curso de Lógica de Programação"
+                className="object-cover rounded-xl"
+                src="/imgs/criptografia.png"
+                width={270}
+                height={270}
+                />
+            </CardHeader>
+            <CardFooter className="flex items-center justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 bg-white ">
+              <h1 className="text-cyan-900 text-bold"> Criptografia</h1>
+            </CardFooter>
+          </Card>
+                </a>
+        </div>
       </div>
       ;
     </>
